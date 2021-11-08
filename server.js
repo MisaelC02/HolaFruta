3b22c3ec1200a70972da7a3233d187a15253be0c
evisintin1 <gtaborda@escuelasproa.edu.ar>
2021-11-08 12:49:38 -0300

    TermineComienzo

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
+
+
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "views"));
+
+
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+
+    res.render('File');
 });
 
 app.listen(port, () => {
