cdfc0b80917d8a51f31353837916b10b4ed16b28
AleisterI <57152858+nahuelonnn@users.noreply.github.com>
2021-11-09 13:04:34 -0300

    TarjetasListas

--- server.js
+++ server.js
@@ -4,19 +4,23 @@ const port = 3000;
 const path = require("path");
 
 app.use(express.static('public'));
-
 app.set("view engine", "ejs");
-
 app.set("views", path.join(__dirname, "views"));
 
 let frutas = 
-[{Fruta:"Manzana", Tipo: "Fruta", Stock: "true"}, 
-{Fruta: "Banana", Tipo: "Fruta", Stock:"true"},
+[{Fruta:"Manzana", Tipo: "Fruta", Stock: "true", Ruta: "img/manzana.jpg"}, 
+{Fruta: "Banana", Tipo: "Fruta", Stock:"true", Ruta: "img/Banana.jpg"},
+{Fruta: "Pera", Tipo: "Fruta", Stock:"true", Ruta: "img/Pera.jpg"},
+{Fruta: "Sandia", Tipo: "Fruta", Stock:"true", Ruta: "img/Sandia.jpg"},
+{Fruta: "Frutilla", Tipo: "Fruta", Stock:"true", Ruta: "img/Frutilla.jpg"},
+{Fruta: "Uva", Tipo: "Fruta", Stock:"true", Ruta: "img/Uva.jpg"},
+{Fruta: "Naranja", Tipo: "Fruta", Stock:"true", Ruta: "img/Naranja.jpg"},
+{Fruta: "Pera", Tipo: "Fruta", Stock:"False"},
 {Fruta: "Pera", Tipo: "Fruta", Stock:"False"}];
+let largo = frutas.length;
 
-app.get("/", (req, res) => {
-
-    res.render('prueba');
+app.get("/verduleria", (req, res) => {
+    res.render('prueba', {frutas:frutas, largo:largo});
 });
 
 
