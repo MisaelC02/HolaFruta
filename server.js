55072dc0d7b23f1f95dfc56c58ee916932d50ed6
AleisterI <57152858+nahuelonnn@users.noreply.github.com>
2021-11-09 09:09:47 -0300

    FirstImg

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 const path = require("path");
 
+app.use(express.static('public'));
 
 app.set("view engine", "ejs");
 
@@ -13,6 +14,10 @@ app.set("views", path.join(__dirname, "views"));
 app.get("/", (req, res) => {
 
     res.render('File');
+});
+app.get("/hola", (req, res) => {
+
+    res.render("Hello Word!");
 });
 
 app.listen(port, () => {
