ed20012f006631f467e5ec1f622f991bf5fc80e4
Misa <91500465+MisaelC02@users.noreply.github.com>
2021-11-12 13:19:42 -0300

    Stocks

--- server.js
+++ server.js
@@ -3,26 +3,31 @@ const app = express();
 const port = 3000;
 const path = require("path");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-let frutas = 
-[{Fruta:"Manzana", Tipo: "Fruta", Stock: "true", Ruta: "img/manzana.jpg"}, 
-{Fruta: "Banana", Tipo: "Fruta", Stock:"true", Ruta: "img/Banana.jpg"},
-{Fruta: "Pera", Tipo: "Fruta", Stock:"true", Ruta: "img/Pera.jpg"},
-{Fruta: "Sandia", Tipo: "Fruta", Stock:"true", Ruta: "img/Sandia.jpg"},
-{Fruta: "Frutilla", Tipo: "Fruta", Stock:"true", Ruta: "img/Frutilla.jpg"},
-{Fruta: "Uva", Tipo: "Fruta", Stock:"true", Ruta: "img/Uva.jpg"},
-{Fruta: "Naranja", Tipo: "Fruta", Stock:"true", Ruta: "img/Naranja.jpg"},
-{Fruta: "Pera", Tipo: "Fruta", Stock:"False"},
-{Fruta: "Pera", Tipo: "Fruta", Stock:"False"}];
+let frutas = [
+    { Fruta: "Manzana", Tipo: "Fruta", Stock: "true", Ruta: "img/manzana.jpg" },
+    { Fruta: "Banana", Tipo: "Fruta", Stock: "true", Ruta: "img/Banana.jpg" },
+    { Fruta: "Pera", Tipo: "Fruta", Stock: "true", Ruta: "img/Pera.jpg" },
+    { Fruta: "Sandia", Tipo: "Fruta", Stock: "true", Ruta: "img/Sandia.jpg" },
+    {
+        Fruta: "Frutilla",
+        Tipo: "Fruta",
+        Stock: "true",
+        Ruta: "img/Frutilla.jpg",
+    },
+    { Fruta: "Uva", Tipo: "Fruta", Stock: "true", Ruta: "img/Uva.jpg" },
+    { Fruta: "Naranja", Tipo: "Fruta", Stock: "true", Ruta: "img/Naranja.jpg" },
+    { Fruta: "Pera", Tipo: "Fruta", Stock: "False" },
+    { Fruta: "Pera", Tipo: "Fruta", Stock: "False" },
+];
 let largo = frutas.length;
 
 app.get("/verduleria", (req, res) => {
-    res.render('prueba', {frutas:frutas, largo:largo});
+    res.render("File", { frutas: frutas, largo: largo });
 });
-
 
 app.listen(port, () => {
     console.log(`El puerto es  http://localhost:${port}`);
