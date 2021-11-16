bbff6063fb1b2cba98aabebc62a3f596ad34b614
AleisterI <91500465+MisaelC02@users.noreply.github.com>
2021-11-16 09:52:25 -0300

    FondoListo

--- server.js
+++ server.js
@@ -10,18 +10,16 @@ app.set("views", path.join(__dirname, "views"));
 let frutas = [
     { Fruta: "Manzana", Tipo: "Fruta", Stock: "true", Ruta: "img/manzana.jpg" },
     { Fruta: "Banana", Tipo: "Fruta", Stock: "true", Ruta: "img/Banana.jpg" },
-    { Fruta: "Pera", Tipo: "Fruta", Stock: "true", Ruta: "img/Pera.jpg" },
+    { Fruta: "Pera", Tipo: "Fruta", Stock: "false", Ruta: "img/Pera.jpg" },
     { Fruta: "Sandia", Tipo: "Fruta", Stock: "true", Ruta: "img/Sandia.jpg" },
     {
         Fruta: "Frutilla",
         Tipo: "Fruta",
-        Stock: "true",
+        Stock: "false",
         Ruta: "img/Frutilla.jpg",
     },
     { Fruta: "Uva", Tipo: "Fruta", Stock: "true", Ruta: "img/Uva.jpg" },
-    { Fruta: "Naranja", Tipo: "Fruta", Stock: "true", Ruta: "img/Naranja.jpg" },
-    { Fruta: "Pera", Tipo: "Fruta", Stock: "False" },
-    { Fruta: "Pera", Tipo: "Fruta", Stock: "False" },
+    { Fruta: "Naranja", Tipo: "Fruta", Stock: "false", Ruta: "img/Naranja.jpg" }
 ];
 let largo = frutas.length;
 
